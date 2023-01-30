c7436c27faf2e2099d01e3b7872c2ce2c8ecb376
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2023-01-30 11:24:09 +0300

    Create filter model for view

---
+++ InnoGotchiFrontend/InnoGotchi.Web/Models/FilterPetViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace InnoGotchi.Web.Models
+{
+    public class FilterPetViewModel
+    {
+        public string SelectedAge { get; private set; }
+        public int HungerLavel { get; private set; }
+        public int ThirstyLavel { get; private set; }
+        public FilterPetViewModel(string selectedAge, int hungerLavel, int thirstyLavel)
+        {
+            SelectedAge = selectedAge;
+            HungerLavel = hungerLavel;
+            ThirstyLavel = thirstyLavel;
+        }
+    }
+}
