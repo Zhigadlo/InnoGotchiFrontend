3564d296c89e384936b13c4b7822193d10907da6
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2023-01-30 11:21:19 +0300

    Add new entities

---
+++ InnoGotchiFrontend/InnoGotchi.BLL/DTO/PaginatedListDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InnoGotchi.BLL.DTO
+{
+    public class PaginatedListDTO<T> where T : class
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public List<T>? Items { get; set; } = null;
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+    }
+}
